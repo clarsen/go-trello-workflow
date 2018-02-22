93bc3b012b723f855b7cb3bd64903a93c9b2d833
Case Larsen <clarsen@gmail.com>
2018-02-21 20:36:45 -0800

    reminder of due soon

--- workflow/reminder.go
+++ workflow/reminder.go
@@ -4,12 +4,21 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"time"
 
 	"github.com/clarsen/trello"
 
 	sendgrid "github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
+
+func formatAsDate(t *time.Time) string {
+	local, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		return "couldn't load timezone"
+	}
+	return t.In(local).Format("2006-01-02 (Mon)")
+}
 
 // MorningRemind generates and sends an email reminding of weekly goals, daily
 // todos, overdue cards
@@ -37,23 +46,61 @@ func MorningRemind(user, appkey, authtoken, sendgridAPIKey, userEmail string) er
 		return err
 	}
 
+	// overdue check
+	overdueBoardsAndLists := []boardAndList{
+		{"Kanban daily/weekly", "Today"},
+		{"Kanban daily/weekly", "Waiting on"},
+		{"Backlog (Personal)", "Backlog"},
+		{"Backlog (Personal)", "Projects"},
+		{"Backlog (work)", "Backlog"},
+		{"Periodic board", "Often"},
+		{"Periodic board", "Weekly"},
+		{"Periodic board", "Bi-weekly to monthly"},
+		{"Periodic board", "Quarterly to Yearly"},
+	}
+
+	now := time.Now()
+
+	var dueSoon []*trello.Card
+	for _, boardlist := range overdueBoardsAndLists {
+		list, err2 := listFor(cl.member, boardlist.Board, boardlist.List)
+		if err2 != nil {
+			// handle error
+			return err2
+		}
+
+		cards, err2 := list.GetCards(trello.Defaults())
+		if err2 != nil {
+			// handle error
+			return err2
+		}
+		for _, card := range cards {
+			if card.Due != nil && now.After(card.Due.Add(-time.Hour*3*24)) {
+				dueSoon = append(dueSoon, card)
+			}
+		}
+	}
+
 	from := mail.NewEmail("Daily reminder", userEmail)
 	subject := fmt.Sprintf("Reminder for %d week %d", summary.Year, summary.Week)
 	to := mail.NewEmail("Goal seeker", userEmail)
-	var content bytes.Buffer
-	t, err := template.ParseFiles("templates/morning-reminder.txt")
-	if err != nil {
-		return err
+
+	fmap := template.FuncMap{
+		"formatAsDate": formatAsDate,
 	}
+	t := template.Must(template.New("morning-reminder.txt").Funcs(fmap).ParseFiles("templates/morning-reminder.txt"))
 
 	data := struct {
 		Summary *WeeklySummary
 		Today   []*trello.Card
+		DueSoon []*trello.Card
 	}{
 		summary,
 		todayCards,
+		dueSoon,
 	}
 
+	var content bytes.Buffer
 	err = t.Execute(&content, data)
 	if err != nil {
 		return err
