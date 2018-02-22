5b816930009fdba8735250d011568edb2d3c77f5
Case Larsen <clarsen@gmail.com>
2018-02-21 19:42:45 -0800

    reminder of today cards.

--- workflow/reminder.go
+++ workflow/reminder.go
@@ -5,13 +5,35 @@ import (
 	"fmt"
 	"html/template"
 
+	"github.com/clarsen/trello"
+
 	sendgrid "github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
+// MorningRemind generates and sends an email reminding of weekly goals, daily
+// todos, overdue cards
 func MorningRemind(user, appkey, authtoken, sendgridAPIKey, userEmail string) error {
+	cl, err := New(user, appkey, authtoken)
+	if err != nil {
+		return err
+	}
+
 	summary, err := GetSummaryForWeek(user, appkey, authtoken)
 	if err != nil {
+		return err
+	}
+
+	// today cards
+	list, err := listFor(cl.member, "Kanban daily/weekly", "Today")
+	if err != nil {
+		// handle error
+		return err
+	}
+
+	todayCards, err := list.GetCards(trello.Defaults())
+	if err != nil {
+		// handle error
 		return err
 	}
 
@@ -23,10 +45,22 @@ func MorningRemind(user, appkey, authtoken, sendgridAPIKey, userEmail string) er
 	if err != nil {
 		return err
 	}
-	err = t.Execute(&content, summary)
+
+	data := struct {
+		Summary *WeeklySummary
+		Today   []*trello.Card
+	}{
+		summary,
+		todayCards,
+	}
+
+	err = t.Execute(&content, data)
 	if err != nil {
 		return err
 	}
+	// for testing
+	// log.Println(content.String())
+	// return nil
 
 	mailcontent := mail.NewContent("text/plain", content.String())
 	m := mail.NewV3MailInit(from, subject, to, mailcontent)
