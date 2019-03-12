f30ece327a803a8791f04edd7901427c488bb538
Case Larsen <clarsen@gmail.com>
2019-03-12 12:00:12 -0700

    expose secrets to graphql handler

--- server/go/handle_graphql/server/main.go
+++ server/go/handle_graphql/server/main.go
@@ -16,7 +16,7 @@ const defaultPort = "8080"
 func addCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Required for CORS support to work
-		w.Header().Set("Access-Control-Allow-Origin", "https://https://workflow.app.caselarsen.com")
+		w.Header().Set("Access-Control-Allow-Origin", "https://workflow.app.caselarsen.com")
 		if strings.HasPrefix(r.Header.Get("Origin"), "http://localhost") {
 			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		}
