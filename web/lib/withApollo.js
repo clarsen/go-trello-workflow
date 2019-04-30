0638fd8ad4ff4ae716aae9a6fdfcc136b03cdded
Case Larsen <clarsen@gmail.com>
2019-04-30 15:05:29 -0700

    not seeing fetch any more…

--- web/lib/withApollo.js
+++ web/lib/withApollo.js
@@ -1,4 +1,5 @@
 import withApollo from 'next-with-apollo'
+import 'cross-fetch/polyfill';
 import ApolloClient, { InMemoryCache }  from 'apollo-boost'
 import { ENDPOINTS } from './api'
 import auth from './auth0'
@@ -6,6 +7,7 @@ import auth from './auth0'
 export default withApollo(({ ctx, headers, initialState }) => (
   new ApolloClient({
     uri: ENDPOINTS['go']['private_gql'],
+    fetch: fetch,
     cache: new InMemoryCache().restore(initialState || {}),
     request: operation => {
       operation.setContext(context => ({
