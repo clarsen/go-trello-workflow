9108826ab0e4f45969369e0353bfa878eeef5113
Case Larsen <clarsen@gmail.com>
2019-05-02 12:59:32 -0700

    use real API

--- web/src/lib/api.js
+++ web/src/lib/api.js
@@ -1,7 +1,7 @@
 export const ENDPOINTS = {
   'go': {
-    'private_gql': 'http://localhost:8080/api/gql',
-    // 'private_gql': 'https://workflow.api.caselarsen.com/api/gql',
+    // 'private_gql': 'http://localhost:8080/api/gql',
+    'private_gql': 'https://workflow.api.caselarsen.com/api/gql',
   },
   'python': {
     'private_gql': 'https://s0veshh554.execute-api.us-east-1.amazonaws.com/dev/api/graphql',
