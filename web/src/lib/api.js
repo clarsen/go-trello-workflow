a964645af32d75b5e2ea33a72cfdd7d20eeab7d4
Case Larsen <clarsen@gmail.com>
2019-11-25 21:35:52 -0800

    remove api and old UI call

--- web/src/lib/api.js
+++ web/src/lib/api.js
@@ -1,10 +1,11 @@
 export const ENDPOINTS = {
   'go': {
+    // 'private_gql': 'http://192.168.7.26:8080/api/gql',
     // 'private_gql': 'http://localhost:8080/api/gql',
     'private_gql': 'https://workflow.api.caselarsen.com/api/gql',
   },
   'python': {
     'private_gql': 'https://s0veshh554.execute-api.us-east-1.amazonaws.com/dev/api/graphql',
-    'timevis_api': 'https://s0veshh554.execute-api.us-east-1.amazonaws.com/dev/api/private',
+    // 'timevis_api': 'https://s0veshh554.execute-api.us-east-1.amazonaws.com/dev/api/private',
   }
 }
